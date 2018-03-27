a06c0ca08376ab6ef14cf4e57f2916e357d86148
Cece Hedrick <cecehedrick@gmail.com>
2018-03-27 00:11:12 -0600

    adds sample project number two

---
+++ LibNumberTwo/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="LibNumberTwo",
+    version='0.01',
+    url="https://github.com/piqueen314/pythonMultipleProjectsOneRepoExample.git",
+    author="Cece Hedrick",
+    author_email="cecehedrick@gmail.com",
+    description="A test python package that has one function that report its name",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
